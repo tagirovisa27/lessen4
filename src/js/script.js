a19cc70510765e35b080ca54d5509f1670d47a24
isa <isatagirov01@mail.ru>
2023-09-03 12:05:22 +0300

    js 9

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,10 @@
-const input = document.querySelector('#myInput');
-
-const button = document.querySelector('#btn-two');
-button.addEventListener('click', function() {
-  
-  if (input.value === "") {
-   block.innerHTML = "Поле обязательно для заполнения";
-}
-  else if (input.value.length < 3) {
-   block.innerHTML = "Поле должно содержать не менее 3-х символов";
-} 
-  else {
-   block.innerHTML = "";
-}
-});
-
-
-const inputTree = document.querySelector('#inp');
-const buttonTree = document.querySelector('#btn-tree');
-
-buttonTree.addEventListener('click', function() {
-
-  if (input == "" || isNaN(input)) {
-    result.innerHTML = "Введите число";
-} 
-  else {
-    result.innerHTML = "Квадрат числа: " + (input * input);
-}
-});
+  $("#btn").click(function() {
+      var inputValue = $("#input").val();
+      if (inputValue === "") {
+          $("#error").text("Поле обязательно для заполнения");
+      } else if (inputValue.length < 3) {
+          $("#error").text("Поле должно содержать не менее 3-х символов");
+      } else {
+          $("#error").text(""); 
+      }
+  }); 
